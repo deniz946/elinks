795771d48cc7898b3c19d35698b10911c89ad7b3
Denis Mehmed <demehmed@everis.com>
2018-07-13 09:30:17 +0200

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CategorySelectorComponent } from './components/category-selector/category-selector.component';
+import { ResourcesTableComponent } from './components/resources-table/resources-table.component';
 
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB1UdXkJ-Ms-Z3TTn9a5WV2ZWuKNh2brvo',
+  authDomain: 'elinks-9d75a.firebaseapp.com',
+  databaseURL: 'https://elinks-9d75a.firebaseio.com',
+  projectId: 'elinks-9d75a',
+  storageBucket: '',
+  messagingSenderId: '462304448901'
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     CategorySelectorComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResourcesTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
